bb992bd6f6d9c8cc7dfde4879cdbed9c8f867253
swade781977 <swade781977@yahoo.com>
2021-04-02 23:44:21 -0400

    commit tree 6d59c548155ccb3fbdf6c1c048c156ca6d0455cc

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => {this.style.color = 'green'}} />
   </div>,
   document.getElementById('root')
 );
